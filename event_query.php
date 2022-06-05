6e47a3952563698e9a5ab4fab61d3bf39828de74
JIAE <58765875+TakJIAe@users.noreply.github.com>
2022-06-06 02:27:09 +0900

    Update event_query.php

--- event_query.php
+++ event_query.php
@@ -13,33 +13,43 @@
     $stmt = $con->prepare($sql);
     $stmt->execute();
 
-    if ($stmt->rowCount() > 0)
+    if($userID != "")
     {
-        
-        $data = array(); 
+        $sql="select * from event where userID='$userID'";
+        $stmt = $con->prepare($sql);
+        $stmt->execute();
 
-        while($row=$stmt->fetch(PDO::FETCH_ASSOC))
-        {
-            extract($row);
-    
-            array_push($data, 
-                array(
-                'title'=>$title,
-                'startdate'=>$startdate,
-                'enddate'=>$enddate
-            ));
-        }
-
-        if (!$android) {
-            echo "<pre>";
-            print_r($data);
-            echo '</pre>';
+        if($stmt->rowCount() == 0){
+            echo "'";
+            echo $userID;
+            echo "'은 찾을 수 없습니다.";
         }
         else{
-            header('Content-Type: application/json; charset=utf8');
-            $json = json_encode(array("root"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
-            echo $json;
+            $data = array(); 
+
+            while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+            {
+                extract($row);
+        
+                array_push($data, 
+                    array(
+                    'ID'=>$ID,
+                    'title'=>$title,
+                    'startdate'=>$startdate,
+                    'enddate'=>$enddate
+                ));
+            }
+
+            if (!$android) {
+                echo "<pre>";
+                print_r($data);
+                echo '</pre>';
+            }
+            else{
+                header('Content-Type: application/json; charset=utf8');
+                $json = json_encode(array("root"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
+                echo $json;
+            }
         }
     }
-
 ?>
