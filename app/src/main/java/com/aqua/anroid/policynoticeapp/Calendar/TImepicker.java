9975fdf40bfd3868635167fafefb8598a2525b3e
TakJIAe <hiworldd@naver.com>
2022-06-09 17:19:08 +0900

    alarmactive

---
+++ app/src/main/java/com/aqua/anroid/policynoticeapp/Calendar/TImepicker.java
@@ -0,0 +1,24 @@
+package com.aqua.anroid.policynoticeapp.Calendar;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class TImepicker extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog( Bundle savedInstanceState) {
+        Calendar c= Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), (TimePickerDialog.OnTimeSetListener) getActivity(), hour, minute, DateFormat.is24HourFormat(getActivity()));
+    }
+}
