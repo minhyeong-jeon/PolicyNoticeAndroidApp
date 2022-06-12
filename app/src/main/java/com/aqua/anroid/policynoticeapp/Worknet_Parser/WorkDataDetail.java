69895de11b9a7f4dc32e1486ab6a855260806144
babanananana <babanananana@naver.com>
2022-06-13 00:13:20 +0900

    commentadd

--- app/src/main/java/com/aqua/anroid/policynoticeapp/Worknet_Parser/WorkDataDetail.java
+++ app/src/main/java/com/aqua/anroid/policynoticeapp/Worknet_Parser/WorkDataDetail.java
@@ -1,15 +1,15 @@
 package com.aqua.anroid.policynoticeapp.Worknet_Parser;
 
 public class WorkDataDetail {
-    public String jobsNm  ;
-    public String wantedTitle;
-    public String relJobsNm;
-    public String jobCont;
-    public String salTpNm;
-    public String workRegion;
-    public String workdayWorkhrCont;
-    public String pfCond;
-    public String selMthd;
+    public String jobsNm  ;          //모집점종
+    public String wantedTitle;      //구인제목
+    public String relJobsNm;        //관련직종
+    public String jobCont;          //직무내용
+    public String salTpNm;          //인금조건
+    public String workRegion;       //근무예정지
+    public String workdayWorkhrCont;//근무시간/형태
+    public String pfCond;           //우대조건
+    public String selMthd;          //전형방법
 
     public WorkDataDetail(){
 
--- app/src/main/java/com/aqua/anroid/policynoticeapp/Worknet_Parser/WorkWantedDetail.java
+++ app/src/main/java/com/aqua/anroid/policynoticeapp/Worknet_Parser/WorkWantedDetail.java
@@ -2,8 +2,8 @@ package com.aqua.anroid.policynoticeapp.Worknet_Parser;
 
 public class WorkWantedDetail {
     public String callTp            = "D";      //페이지타입 L:목록 D상세
-    public String returnType="XML";
-    public String wantedAuthNo="";
-    public String infoSvc="VALIDATION";
-    public String servID            = "";
+    public String returnType="XML";   //리턴타입
+    public String wantedAuthNo="";    //구인인증번호
+    public String infoSvc="VALIDATION";    //정보제공처 지정
+    public String servID            = "";  //서비스아이디
 }
