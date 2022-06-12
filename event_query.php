41f85d8ec6b48912148c156ff82d7b6de49ab7e4
JIAE <58765875+TakJIAe@users.noreply.github.com>
2022-06-12 14:44:08 +0900

    Update event_query.php

--- event_query.php
+++ event_query.php
@@ -4,17 +4,19 @@
     ini_set('display_errors',1); 
 
     include('dbcon.php');
-        
+    
+    // 안드로이드 코드에서 postParameters에 적은 값으로 데이터를 전달 받는다
     $userID = isset($_POST['userID']) ? $_POST['userID'] : '';
     $android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
 
-
+    //ID가 일치하는 event 테이블의 모든 정보를 조회한다.
     $sql="select * from event where userID='$userID'";
     $stmt = $con->prepare($sql);
     $stmt->execute();
 
     if($userID != "")
     {
+        //ID가 일치하는 event 테이블의 모든 정보를 조회한다.
         $sql="select * from event where userID='$userID'";
         $stmt = $con->prepare($sql);
         $stmt->execute();
@@ -31,12 +33,15 @@
             {
                 extract($row);
         
+                //array로 데이터 전송
                 array_push($data, 
                     array(
                     'ID'=>$ID,
                     'title'=>$title,
                     'startdate'=>$startdate,
-                    'enddate'=>$enddate
+                    'enddate'=>$enddate,
+                    'alarmactive'=>$alarmactive
+
                 ));
             }
 
