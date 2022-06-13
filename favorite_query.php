36ea182c04aea6bcc141efbf8ae8156610fb0cde
yejin <95522037+jinjin999@users.noreply.github.com>
2022-06-13 20:09:00 +0900

    Update favorite_query.php

--- favorite_query.php
+++ favorite_query.php
@@ -6,17 +6,15 @@
     include('dbcon.php');
 
     //POST 값을 읽어온다.
-    // $item_content=isset($_POST['item_content']) ? $_POST['item_content'] : '';
-    // $item_name = isset($_POST['item_name']) ? $_POST['item_name'] : '';
-
     $userID = isset($_POST['userID']) ? $_POST['userID'] : '';
     $android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
 
-    
+    //mysql의 favorite 테이블에서 usreID가 같은 데이터를 검색한다.
     $sql="select * from favorite where userID='$userID'";
     $stmt = $con->prepare($sql);
     $stmt->execute();
     
+    //favorite 테이블에 해당 userID로 저장된 데이터가 없을 경우
     if ($stmt->rowCount() == 0){
 
         echo "즐겨찾기를 추가해주세요.";
@@ -26,6 +24,7 @@
         while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
 
             extract($row);
+                //배열에 저장한다.
                 array_push($data, 
                     array('id'=>$row["id"],
                     'item_name'=>$row["item_name"],
@@ -37,9 +36,10 @@
             }
 
 
-        //안드로이드에 전달하기 위해 JSON 포맷으로 변경 후 에코한다.
+        //안드로이드에 전달하기 위해 JSON 포맷으로 변경 후 에코
         if (!$android) {
             echo "<pre>";
+            //배열을 출력한다.
             print_r($data);
             echo '</pre>';
         }
@@ -51,4 +51,3 @@
     }
 
 ?>
-
