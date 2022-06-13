9bfc9f7f63ee26009697df788ce23a479bbf80a6
yejin <95522037+jinjin999@users.noreply.github.com>
2022-06-13 20:08:43 +0900

    Update favorite_delete.php

--- favorite_delete.php
+++ favorite_delete.php
@@ -9,14 +9,14 @@
     $item_name = isset($_POST['item_name']) ? $_POST['item_name'] : '';
     $android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
 
-
+    //mysql의 favorite 테이블에서 item_name이 같은 데이터를 검색한다.
     $sql="select * from favorite where item_name='$item_name'";
     $stmt = $con->prepare($sql);
     $stmt->execute();
 
     try{
+        //mysql의 favorite 테이블에서 item_name이 같은 데이터를 찾아 삭제한다.
         $sql = "delete from favorite where item_name='$item_name'";
-        //sql문 실행하여 데이터를 MYSQL 서버의 favorite 테이블에서 제거
         $stmt = $con->prepare($sql);
         $stmt->execute();
     } catch(PDOException $e) {
